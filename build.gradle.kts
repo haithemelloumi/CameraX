4d4de0ce2b71b00ff00900a44e3f67079d46a28d
Haithem Elloumi <elloumi.haithem@gmail.com>
2024-09-11 12:28:17 +0200

    [TECH] integrate Dagger Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    // dagger hilt
+    alias(libs.plugins.dagger.hilt.android) apply false
+    // ksp
+    alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
 }
