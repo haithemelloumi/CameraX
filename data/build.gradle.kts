644f4cbf236d2c9e00143fe94c4083103ba6e50e
Haithem Elloumi <elloumi.haithem@gmail.com>
2024-09-11 12:51:33 +0200

    [TECH] Setting up the modules

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    // dagger hilt
+    alias(libs.plugins.dagger.hilt.android)
+    // ksp
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
-    namespace = "com.helloumi.ui"
+    namespace = "com.helloumi.data"
     compileSdk = 34
 
     defaultConfig {
@@ -30,41 +34,26 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-    // AndroidX
-    implementation(libs.appcompat)
-    implementation(libs.core.ktx)
-    implementation(libs.constraintlayout)
+    // Dagger Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
-    // Navigation
-    implementation(libs.navigation.fragment.ktx)
-    implementation(libs.navigation.ui.ktx)
-
-    // CameraX
-    implementation(libs.camera.core)
-    implementation(libs.camera.camera2)
-    implementation(libs.camera.lifecycle)
-    implementation(libs.camera.video)
-    implementation(libs.camera.view)
-    implementation(libs.camera.extensions)
+    // Json
+    implementation(libs.moshi)
+    implementation(libs.moshi.adapters)
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.kotlin.codegen)
+    implementation(libs.converter.moshi)
+    api(libs.gson)
 
     // Testing
     implementation(libs.junit.ktx)
-    androidTestImplementation(libs.junit.ktx)
     testImplementation(libs.kotlinx.coroutines.test)
-    androidTestImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockito.inline)
     testImplementation(libs.mockito.kotlin)
-    androidTestImplementation(libs.mockito.android)
-    androidTestImplementation(libs.mockito.core)
-    androidTestImplementation(libs.espresso.core)
     testImplementation(libs.kotlin.test.junit)
     testImplementation(libs.mockk)
-    androidTestImplementation(libs.mockk.android)
 }
